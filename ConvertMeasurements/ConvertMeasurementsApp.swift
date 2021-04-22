7dfd1ed87e68676cb472bd2d0086a6e7191f5c34
William Hatzar <i>
2021-04-22 20:11:53 +0100

    Initial Commit

---
+++ ConvertMeasurements/ConvertMeasurementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertMeasurementsApp.swift
+//  ConvertMeasurements
+//
+//  Created by William Hatzar on 22/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct ConvertMeasurementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
